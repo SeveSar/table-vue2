8ad8665658a4ff31ba507f62b71f7079115eab9d
Sevesar7337 <averin.dev@mail.ru>
2022-12-20 14:10:00 +0500

    changed name of project

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/test-table-vue/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/table-vue2/' : '/',
   css: {
     loaderOptions: {
       sass: {
